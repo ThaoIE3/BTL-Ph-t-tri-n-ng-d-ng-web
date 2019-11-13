528b75acd3ed65953889c27f73483c8d791f43c3
ThaoIE3 <trinhthithao1999@gmail.com>
2019-11-14 01:05:16 +0700

    thêm file in môn học, chuyển hướng trang, sửa phần login

---
+++ btl-ver2/in lịch thi.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>in lịch thi</title>
+	<link rel="stylesheet" type="text/css" href="theme/frontend/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="theme/frontend/css/style.css">
+</head>
+<body>
+	<div class="head row">
+		<div class="col-lg-6 col-md-6 col-sm-6">
+			<p>ĐẠI HỌC QUỐC GIA HÀ NỘI</p>
+			<p class="line-2">TRƯỜNG ĐẠI HỌC CÔNG NGHỆ</p>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6">
+			<p>CỘNG HÒA XÃ HỘI CHỦ NGHĨA VIỆT NAM</p>
+			<p class="line-2">Độc lập - Tự do - Hạnh phúc</p>
+		</div>
+	</div>
+	<div class="main-title"><h1>lịch thi học kỳ I, năm học 2019-2020</h1></div>
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-sm-4">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<p>Họ và tên:</p>
+					<p>Khoa: </p>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<p>Trịnh Thị Thảo</p>
+					<p>Công nghệ thông tin</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<p>Ngày sinh:</p>
+					<p>Khóa: </p>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<p>10/09/1999</p>
+					<p>QH-2017-I/CQ</p>
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4">
+			<div class="row">
+				<div class="col-lg-3 col-md-3 col-sm-3">
+					<p>MSSV:</p>
+					<p>Lớp: </p>
+				</div>
+				<div class="col-lg-9 col-md-9 col-sm-9">
+					<p>17021025</p>
+					<p>QH-2017-I/CQ-CD</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th scope="col">Số thứ tự</th>
+				<th scope="col">Mã học phần</th>
+				<th scope="col">Môn thi</th>
+				<th scope="col">Ngày thi</th>
+				<th scope="col">Ca thi(giờ thi)</th>
+				<th scope="col">Hình thức thi</th>
+				<th scope="col">Phòng thi</th>
+				<th scope="col">Số báo danh</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">1</th>
+				<td>182-ELT2028-01</td>
+				<td>Chuyên nghiệp trong công nghệ</td>
+				<td>25/05/2019</td>
+				<td>1 (08:00)</td>
+				<td>Viết</td>
+				<td>304 GĐ2</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<th scope="row">2</th>
+				<td>182-ELT2028-01</td>
+				<td>Trí tuệ nhân tạo</td>
+				<td>25/05/2019</td>
+				<td>1 (08:00)</td>
+				<td>Viết</td>
+				<td>304 GĐ2</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<th scope="row">3</th>
+				<td>182-INT3306-21</td>
+				<td>Phát triển ứng dụng web</td>
+				<td>25/05/2019</td>
+				<td>1 (08:00)</td>
+				<td>Viết</td>
+				<td>304 GĐ2</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<th scope="row">4</th>
+				<td>182-ELT2028-01</td>
+				<td>Thu thập và phân tích yêu cầu</td>
+				<td>25/05/2019</td>
+				<td>1 (08:00)</td>
+				<td>Viết</td>
+				<td>304 GĐ2</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<th scope="row">5</th>
+				<td>182-ELT2028-01</td>
+				<td>Kiểm thử</td>
+				<td>25/05/2019</td>
+				<td>1 (08:00)</td>
+				<td>Viết</td>
+				<td>304 GĐ2</td>
+				<td>20</td>
+			</tr>
+		</tbody>
+	</table>
+	<button type="submit">TẢI XUỐNG</button>
+
+	<script src="theme/frontend/js/jquery.min.js" defer></script>
+	<script src="theme/frontend/js/bootstrap.min.js" defer></script>
+	<script src="theme/frontend/js/script.js" defer></script>
+</body>
+</html>
---
+++ btl-ver2/Đăng kí thi.php
@@ -0,0 +1,327 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Đăng kí thi</title>
+	<link rel="stylesheet" type="text/css" href="theme/frontend/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="theme/frontend/css/style.css">
+</head>
+<body>
+	<header>
+		<a href="#" title>
+			<img src="theme/frontend/images/logo.png">
+		</a>
+		<div class="header-title">
+			<h3>cổng thông tin đào tạo đại học</h3>
+			<h4>Dành cho sinh viên</h4>
+		</div>
+		<div class="welcome">
+			Xin chào bạn: <strong>Trịnh Thị Thảo </strong>[17021025]
+		</div>
+		<div class="bannerMenu">
+			<ul>
+				<li>
+					Chọn học kì: 
+					<select>
+						<option selected="">Học kì 1 năm 2019 - 2020</option>
+						<option>Học kì 2 năm 2018 - 2019</option>
+						<option>Học kì 1 năm 2018 - 2019</option>
+						<option>Học kì 2 năm 2017 - 2018</option>
+						<option>Học kì 1 năm 2017 - 2018</option>
+					</select>
+				</li>
+				<li class="see-the-notice">
+					<a class="smooth" href="#" title>Xem thông báo</a>
+				</li>
+				<li class="change-pass">
+					<a class="smooth" href="#" title>Đổi mật khẩu</a>
+				</li>
+				<li class="exit">
+					<a class="smooth" href="#" title>Thoát</a>
+				</li>
+			</ul>
+		</div>
+	</header>
+	<div class="main">
+		<div class="left-list">
+			<table>
+				<tbody>
+					<tr>
+						<td><a href="http://localhost/btl-ver2/home.php">» Hướng dẫn</a></td>
+					</tr>
+					<tr>
+						<td>» Cập nhật hồ sơ sinh viên</td>
+					</tr>
+					<tr>
+						<td>» Kết quả đăng kí học</td>
+					</tr>
+					<tr>
+						<td>» Đăng kí môn học</td>
+					</tr>
+					<tr>
+						<td>» Kết quả học tập</td>
+					</tr>
+					<tr>
+						<td class="first-news">» Lịch thi</td>
+					</tr>
+					<tr>
+						<td>» Đề cương môn học</td>
+					</tr>
+					<tr>
+						<td>» Danh sách biểu mẫu</td>
+					</tr>
+					<tr>
+						<td>» Gửi yêu cầu trợ giúp</td>
+					</tr>
+					<tr>
+						<td>» Tài khoản truy cập email</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="right-content">
+			<div class="guide">
+				<h5>Lịch thi</h5>
+			</div>
+			<div class="notification">
+				<h3>XEM ĐĂNG KÝ THI HỌC KỲ I, NĂM HỌC 2019-2020</h3>
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-4">
+						Chọn: 
+						<select>
+							<option>Xem lịch thi</option>
+							<option selected="">Đăng kí lịch thi</option>
+						</select>
+						<p>Mã sinh viên: 17021025</p>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-4">
+						Học kì: 
+						<select>
+							<option>--Học kỳ--</option>
+							<option selected="">Học kì 1 năm 2019 - 2020</option>
+							<option>Học kì 2 năm 2018 - 2019</option>
+							<option>Học kì 1 năm 2018 - 2019</option>
+							<option>Học kì 2 năm 2017 - 2018</option>
+							<option>Học kì 1 năm 2017 - 2018</option>
+						</select>
+						<p>Họ và tên: Trịnh Thị Thảo</p>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-4">
+						<div class="sum-exam">Tổng số kì thi đăng kí: 0</div>
+					</div>
+				</div>
+				<div class="list-exam">
+					<table class="table table-bordered">
+						<thead>
+						    <tr>
+						      	<th scope="col">số thứ tự</th>
+						      	<th scope="col">Mã học phần</th>
+						      	<th scope="col">Môn thi</th>
+						      	<th scope="col">Ngày thi</th>
+						      	<th scope="col">Ca thi(giờ thi)</th>
+						      	<th scope="col">Hình thức thi</th>
+						      	<th scope="col">Phòng thi</th>
+						      	<th scope="col">Tổng sinh viên</th>
+						      	<th scope="col">Đã đăng kí</th>
+						      	<th scope="col">Chọn</th>
+						    </tr>
+						</thead>
+						<tbody>
+						    <tr>
+						      	<th scope="row">1</th>
+						      	<td>182-ELT2028-01</td>
+						      	<td>Chuyên nghiệp trong công nghệ</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">2</th>
+						      	<td>182-ELT2028-01</td>
+						      	<td>Chuyên nghiệp trong công nghệ</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">3</th>
+						      	<td>182-ELT2028-01</td>
+						      	<td>Chuyên nghiệp trong công nghệ</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">4</th>
+						      	<td>182-ELT2028-01</td>
+						      	<td>Chuyên nghiệp trong công nghệ</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">5</th>
+						      	<td>182-ELT2028-01</td>
+						      	<td>Chuyên nghiệp trong công nghệ</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">6</th>
+						      	<td>182-ELT2028-01</td>
+						      	<td>Chuyên nghiệp trong công nghệ</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">7</th>
+						      	<td>182-INT3401-21</td>
+						      	<td>Trí tuệ nhân tạo</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">8</th>
+						      	<td>182-INT3401-21</td>
+						      	<td>Trí tuệ nhân tạo</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">9</th>
+						      	<td>182-INT3306-21</td>
+						      	<td>Phát triển ứng dụng web</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">10</th>
+						      	<td>182-INT3306-21</td>
+						      	<td>Phát triển ứng dụng web</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">11</th>
+						      	<td>182-INT3306-21</td>
+						      	<td>Thu thập và phân tích yêu cầu</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						    <tr>
+						      	<th scope="row">12</th>
+						      	<td>182-INT3309-2</td>
+						      	<td>Kiểm thử</td>
+						      	<td>25/05/2019</td>
+						      	<td>1 (08:00)</td>
+						      	<td>Viết</td>
+						      	<td>304 GĐ2</td>
+						      	<td>34</td>
+						      	<td>30</td>
+						      	<td>
+						      		<input type="checkbox" name="">
+						      	</td>
+						    </tr>
+						</tbody>
+					</table>
+					<a class="print smooth" href="http://localhost/btl-ver2/lịch thi bạn đã đăng kí.php" title>Đăng ký</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="row">
+			<div class="col-lg-2 online">
+				Đang online
+				<br>
+				<strong>1</strong>
+			</div>
+			<div class="col-lg-6">
+				<span>©</span>
+				<span>2008-2019</span>
+				<a href="#" title>Trung tâm ứng dụng CNTT - VNU</a>
+				<p>This page was rendered at 11/8/2019 8:59:46 AM by DANGKYTHI-1</p>
+			</div>
+			<div class="col-lg-4 info">
+				<a href="#" title>Trang chủ VNU</a>
+				<a href="#" title>Trang chủ CAIT</a>
+			</div>
+		</div>
+	</footer>
+	<script src="theme/frontend/js/jquery.min.js" defer></script>
+	<script src="theme/frontend/js/bootstrap.min.js" defer></script>
+	<script src="theme/frontend/js/script.js" defer></script>
+</body>
+</html>
